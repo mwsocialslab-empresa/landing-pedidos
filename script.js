b492f557c51149f4515cd37d3dd5235b606f3b0a
walterG <waltergalelab@gmail.com>
2026-01-28 18:45:21 -0300

    aggregamos navbar y oja ofertas

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// 🧾 CARGAR PRODUCTOS
 const URL_SHEETS = "https://script.google.com/macros/s/AKfycbzYZXizwOl7VWgxjTU7LeFzg91HuS8Fh1o4aDDRw4mWjCd8Q4xzRMewc1yNzRkb_PyL_g/exec";
 
 let carrito = [];
@@ -13,21 +15,27 @@ fetch(URL_SHEETS)
 
     for (const categoria in data) {
       data[categoria].forEach(p => {
-        const precio = Number(p.oferta) > 0 ? Number(p.oferta) : Number(p.precio);
+
+        const precioOriginal = Number(p.precio);
+        const precioFinal = Number(p.oferta) > 0 ? Number(p.oferta) : precioOriginal;
 
         productos.push({
           nombre: p.nombre,
-          precio,
+          precio: precioFinal,
+          precioOriginal: precioOriginal,
           unidad: p.unidad
         });
 
         contenedor.innerHTML += `
-          <div class="col-md-4 producto" data-categoria="${categoria}">
+          <div class="col-lg-3 col-md-4 col-sm-6 producto" data-categoria="${categoria}">
             <div class="card h-100 text-center shadow-sm">
               <div class="card-body">
 
                 <h5>${p.nombre}</h5>
-                <p class="text-success fw-bold">$${precio} / ${p.unidad}</p>
+
+                <p class="text-success fw-bold">
+                  $${precioFinal} / ${p.unidad}
+                </p>
 
                 <div class="input-group mb-2">
                   <button class="btn btn-outline-secondary"
@@ -37,14 +45,13 @@ fetch(URL_SHEETS)
                     class="form-control text-center"
                     id="cant${index}"
                     value="0"
-                    readonly>
+                    readonly></input>
 
                   <span class="input-group-text">kg</span>
 
                   <button class="btn btn-outline-secondary"
                     onclick="cambiarCantidad(${index}, 0.5)">+</button>
                 </div>
-
 
                 <p>Subtotal: $<span id="sub${index}">0</span></p>
 
@@ -61,6 +68,7 @@ fetch(URL_SHEETS)
       });
     }
   });
+
 function cambiarCantidad(i, valor) {
   const input = document.getElementById(`cant${i}`);
   let cantidad = parseFloat(input.value) || 0;
@@ -161,3 +169,24 @@ function enviarPedidoWhatsApp() {
     `https://wa.me/5491127461954?text=${encodeURIComponent(msg)}`
   );
 }
+function filtrar(categoria) {
+  const productosDOM = document.querySelectorAll(".producto");
+
+  productosDOM.forEach((p, i) => {
+    if (categoria === "todos") {
+      p.style.display = "block";
+      return;
+    }
+
+    if (categoria === "ofertas") {
+      const tieneOferta = productos[i].precio < productos[i].precioOriginal;
+      p.style.display = tieneOferta ? "block" : "none";
+      return;
+    }
+
+    p.style.display =
+      p.dataset.categoria === categoria ? "block" : "none";
+  });
+}
+
+
